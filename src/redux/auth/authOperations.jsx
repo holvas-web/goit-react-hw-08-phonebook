f5e54ddbf61b2698ac9d3c8bb9e1340ee19a1b13
Vasyl Holubovskyi <66141432+holvas-web@users.noreply.github.com>
2023-10-22 19:41:56 +0200

    fix

--- src/redux/auth/authOperations.jsx
+++ src/redux/auth/authOperations.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import toast from 'react-hot-toast';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
